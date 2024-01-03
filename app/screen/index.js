9184513ef6aeb5834f7a04a6160c88e752db1038
adityarosyitama <addityarosyyitama2@gmail.com>
2024-01-03 13:24:54 +0700

    update general

--- app/screen/index.js
+++ app/screen/index.js
@@ -7,17 +7,15 @@ const PORT = 3000;
 const middlewares = require('../utils/middlewares/middlewares');
 const connectionDB = require("../utils/mongoDB/mongoose");
 
-require('dotenv').config();
-connectionDB();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
-// app.use((req, res, next) => {
-//     console.log('Time: ', Date.now());
-//     next();
-//   });
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+app.listen(PORT, () => console.log(`Run in port http://localhost:${PORT}/`));
+require('dotenv').config();
+connectionDB();
 
 app.get("/", (req, res) => {
     const htmlView = `
@@ -33,16 +31,11 @@ app.get("/", (req, res) => {
     </body>
     </html>
     `;
-
     console.log('Time: ', Date.now());
-
     res.send(htmlView);
 });
 
 app.use('/login', require('./login/index'))
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
-app.listen(PORT, () => console.log(`Run in port http://localhost:${PORT}/`));
 
 module.exports = app
