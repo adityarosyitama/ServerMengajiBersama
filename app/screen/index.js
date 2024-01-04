07ae00953250adedc34f817837386b34e643d20a
adityarosyitama <addityarosyyitama2@gmail.com>
2024-01-04 22:56:04 +0700

    update general

--- app/screen/index.js
+++ app/screen/index.js
@@ -16,6 +16,13 @@ app.use(express.json());
 app.use(express.static('public'));
 app.listen(PORT, () => console.log(`Run in port http://localhost:${PORT}/`));
 
+// // Serve static files (like CSS)
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
 app.get("/", (req, res) => {
   const path = `/api/item/${uuidv4()}`
   const htmlView = `
